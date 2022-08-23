ff78672a12148e2b4dbf6438eaafe0db2a4bc6c2
Sue Young <m.sue.young@gmail.com>
2022-08-23 16:54:22 -0600

    still not working but worth a commit

--- city-explorer/src/App.js
+++ city-explorer/src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+// import Image from 'react-bootstrap/Image';
+import React from 'react';
+//import axios from 'axios';
+//import CityForm from './CityForm.js'
+// import CityInfo from './CityInfo.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and other pieces of code, save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  constructor( props ){
+    super( props )
+    this.state = {
+      sQuery: '',
+      sResults: null,
+      mURL: '',
+      tError: null,
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState ({
+      sQuery: event.target.value,
+    });
+  };
+
+  handleSearch = async (event) => {
+    event.preventDefault()
+    try {
+      // const response = await axios.get(URL);
+      this.setState( {
+        sResults: response.data[0],
+        tError: null,
+      })
+    } catch (error) {
+      this.setState({ tError: error})
+    }
+  };
+
+  render() {
+    return (
+      <>
+      <div className="App">
+        <header className="App-header">
+          <h1> Render something already! </h1>
+        </header>
+
+      </div>
+      </>
+  )}
 }
 
 export default App;
---
+++ city-explorer/src/CityInfo.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Accordion from "react-bootstrap/Accordion";
+
+class CityInfo extends React.Component {
+  constructor(props) {
+    super( props)
+    this.state = { theKey: 0 }
+ }
+
+ handleClick = () => {
+  this.setState( state => {
+    return { theKey: state.theKey ? 0 : 1 }
+  });
+}
+
+  render() {
+    return (
+      <>
+      {
+        this.props.cityInfo && 
+        <div className="CityInfo">
+        {/* //   <Accordion flush activeKey={this.state.theKey} className="CityInfo"/>
+        //     <Accordion.Item theKey={0}>
+        //       <Accordion.Header onClick={this.handleClick} className="CityInfo"
+        //       {this.props.cityInfo?.display_name}
+        //       </Accordian.Header>  
+        //       <Accordian.Body>  
+        //        {this.props.cityInfo?.lat}, {this.props.cityInfo?.lon}
+        //       </Accordian.Body>  
+        //     </Accordian.Item>
+        //   </Accordian>             */}
+        </div>
+      }
+      </>
+    )
+  }
+}
+
+export default CityInfo;
